acaa8ab819cdedc3cfbdde9b80966ba5c4485bee
ivan <ivanacosta0627@gmail.com>
2024-09-17 18:56:35 +0800

    09/17_02_Your First Route and View

--- resources/views/about.php
+++ resources/views/about.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>About Page</title>
 </head>
 <body>
-    <h1>Hello! World!</h1>
+    <h1>Hello from the about page</h1>
 </body>
 </html>
