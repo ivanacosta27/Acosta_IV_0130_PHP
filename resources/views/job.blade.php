1b748697d9d8c5dea3df726422ab9e58a79f02bf
ivan <ivanacosta0627@gmail.com>
2024-10-17 21:00:57 +0800

    08_Introduction to migrations

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -2,10 +2,10 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-    <h2 class="font-bold text-lg">
-        {{ $job['title']}}
-    </h2>
+
+    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+
     <p>
-        This job pays  {{$job['salary']}} per year.
+        This job pays {{ $job['salary'] }} per year.
     </p>
 </x-layout>
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,14 @@
     <x-slot:heading>
         Job Listings
     </x-slot:heading>
+
     <ul>
         @foreach ($jobs as $job)
-        <li>
-            <a href="/jobs/{{ $job['id'] }}" class="text-blue-600 hover:underline">
-                <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
-            </a>
-        </li>
+            <li>
+                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                </a>
+            </li>
         @endforeach
     </ul>
 </x-layout>
